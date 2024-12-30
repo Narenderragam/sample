05dc2946c986ebea6216d71da8aab818e4636b71
Narenderragam <ragamnarender888@gmail.com>
2024-12-30 11:49:48 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,9 @@
+name = "narender"
+age = 22
+print ( name )
+print( age )
+percentage = 95.36
+print( percentage )
+def function1( age ,name):
+   return age,name
+function1( 22,"narender")
---
+++ narender.py
@@ -0,0 +1,8 @@
+print("hello")
+print('vamsi')
+a = 10
+print(a)
+def function1( name , age):
+    return name , age
+ a = function1("narender" , 22)
+ print(a)
